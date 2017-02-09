d3ed00a1009129a2c7ac20873471f6b15aaa5e58
AIE\s171445 <pienation@hotmail.com>
2017-02-09 15:09:24 +1100

    SPAWNING

--- project2D/FoodSpawner.cpp
+++ project2D/FoodSpawner.cpp
@@ -1,9 +1,10 @@
 #include "FoodSpawner.h"
-#include "Food.h"
+#include "Level.h"
+#include <time.h>
 
-FoodSpawner::FoodSpawner()
+FoodSpawner::FoodSpawner(float dt)
 {
-
+	srand(time(NULL));
 }
 
 FoodSpawner::~FoodSpawner()
@@ -13,6 +14,15 @@ FoodSpawner::~FoodSpawner()
 
 void FoodSpawner::Spawn()
 {
+	if (m_DoesFoodExist == false)
+	{
+		Level::SetMap((rand() % 16 + 1), (rand() % 16 + 1), E_LevelSlot_Food);
+		m_DoesFoodExist == true;
+	}
+}
 
+void FoodSpawner::SetFood()
+{
+	m_DoesFoodExist == false;
 }
 
--- project2D/FoodSpawner.h
+++ project2D/FoodSpawner.h
@@ -1,15 +1,26 @@
 #pragma once
+#include <glm\glm.hpp>
+
 #define SizeOfList 16
 
 class FoodSpawner
 {
 public:
 
-	FoodSpawner();
+	FoodSpawner(float dt);
 	~FoodSpawner();
 	
 	void Spawn();
+	void SetFood();
 
 protected:
-	int m_ListOfFood[SizeOfList];
+	int m_FoodObject;
+	
+	bool m_DoesFoodExist;
+
+	glm::vec2 m_MapLocation;
+
+	int m_MapX;
+	int m_MapY;
+
 };
